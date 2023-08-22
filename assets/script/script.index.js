c4335e0bb326d51b871dfd1999a3dc41dfd558a0
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-08-22 11:19:07 -0300

    fix: showlist

--- assets/script/script.index.js
+++ assets/script/script.index.js
@@ -61,8 +61,8 @@ class User {
     }
 
     possible_Client() {
-         possibleClient(this.age);
-     }
+        possibleClient(this.age);
+    }
 }
 
 class AllUsers {
@@ -97,115 +97,119 @@ function createUser() {
                 sendErrorMsg("Alguém já cadastrou este CPF")
             } else {
                 countUsers();
+                showList();
                 clearInputs();
             }
         }
     }
 }
 
-    function valida_cpf(cpf) {
-        console.log("Passou pela funcao valida_cpf()");
+function valida_cpf(cpf) {
+    console.log("Passou pela funcao valida_cpf()");
 
-        var numeros, digitos, soma, i, resultado, digitos_iguais;
-        digitos_iguais = 1;
-        if (cpf.length < 11)
+    var numeros, digitos, soma, i, resultado, digitos_iguais;
+    digitos_iguais = 1;
+    if (cpf.length < 11)
+        return false;
+    for (i = 0; i < cpf.length - 1; i++)
+        if (cpf.charAt(i) != cpf.charAt(i + 1)) {
+            digitos_iguais = 0;
+            break;
+        }
+    if (!digitos_iguais) {
+        numeros = cpf.substring(0, 9);
+        digitos = cpf.substring(9);
+        soma = 0;
+        for (i = 10; i > 1; i--)
+            soma += numeros.charAt(10 - i) * i;
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(0))
             return false;
-        for (i = 0; i < cpf.length - 1; i++)
-            if (cpf.charAt(i) != cpf.charAt(i + 1)) {
-                digitos_iguais = 0;
-                break;
-            }
-        if (!digitos_iguais) {
-            numeros = cpf.substring(0, 9);
-            digitos = cpf.substring(9);
-            soma = 0;
-            for (i = 10; i > 1; i--)
-                soma += numeros.charAt(10 - i) * i;
-            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado != digitos.charAt(0))
-                return false;
-            numeros = cpf.substring(0, 10);
-            soma = 0;
-            for (i = 11; i > 1; i--)
-                soma += numeros.charAt(11 - i) * i;
-            resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-            if (resultado != digitos.charAt(1))
-                return false;
-            return true;
-        }
-        else
+        numeros = cpf.substring(0, 10);
+        soma = 0;
+        for (i = 11; i > 1; i--)
+            soma += numeros.charAt(11 - i) * i;
+        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+        if (resultado != digitos.charAt(1))
             return false;
+        return true;
     }
+    else
+        return false;
+}
 
-    function sendErrorMsg(msg) {
-        console.log("Passou pela funcao sendErrorMsg()");
+function sendErrorMsg(msg) {
+    console.log("Passou pela funcao sendErrorMsg()");
 
-        document.getElementById("error-msg").innerHTML = msg;
-        document.getElementById("error-msg").classList.remove("hidden");
-        setTimeout(function () {
-            document.getElementById("error-msg").classList.add("hidden");
-        }, 4000);
+    document.getElementById("error-msg").innerHTML = msg;
+    document.getElementById("error-msg").classList.remove("hidden");
+    setTimeout(function () {
+        document.getElementById("error-msg").classList.add("hidden");
+    }, 4000);
+}
+
+function sendSuccessMsg(msg) {
+    console.log("Passou pela funcao sendErrorMsg()");
+
+    document.getElementById("success-msg").innerHTML = msg;
+    document.getElementById("success-msg").classList.remove("hidden");
+    setTimeout(function () {
+        document.getElementById("success-msg").classList.add("hidden");
+    }, 4000);
+}
+
+function isAnyInputEmpty() {
+    if (document.getElementById("name").value == "" || document.getElementById("birthdate").value == "" || document.getElementById("address").value == "" || document.getElementById("phone") == "" || document.getElementById("email").value == "" || document.getElementById("cpf").value == "") {
+        return true
+    } else {
+        return false
     }
+}
 
-    function sendSuccessMsg(msg) {
-        console.log("Passou pela funcao sendErrorMsg()");
+function isUserAlreadyRegistered(cpf) {
+    arrayUsers.listUsers.forEach((user) => {
+        console.log(user.cpf);
+    });
+}
 
-        document.getElementById("success-msg").innerHTML = msg;
-        document.getElementById("success-msg").classList.remove("hidden");
-        setTimeout(function () {
-            document.getElementById("success-msg").classList.add("hidden");
-        }, 4000);
+function clearInputs() {
+    document.getElementById("name").value == ""
+    document.getElementById("birthdate").value == ""
+    document.getElementById("address").value == ""
+    document.getElementById("phone") == ""
+    document.getElementById("email").value == ""
+    document.getElementById("cpf").value == "";
+
+    return
+}
+
+function showUsers() {
+    document.getElementById("sub-div").classList.remove("hidden");
+    document.getElementById("main-div").classList.add("hidden");
+}
+
+function showRegister() {
+    document.getElementById("sub-div").classList.add("hidden");
+    document.getElementById("main-div").classList.remove("hidden");
+}
+
+
+function possibleClient(age) {
+    if (age == 18 && age <= 31) {
+        return true
+    } else {
+        return false
     }
+}
 
-    function isAnyInputEmpty() {
-        if (document.getElementById("name").value == "" || document.getElementById("birthdate").value == "" || document.getElementById("address").value == "" || document.getElementById("phone") == "" || document.getElementById("email").value == "" || document.getElementById("cpf").value == "") {
-            return true
-        } else {
-            return false
-        }
-    }
+function countUsers() {
+    document.getElementById("contador").innerHTML = `Contador: ${arrayUsers.listUsers.length}`
+}
 
-    function isUserAlreadyRegistered(cpf) {
-        arrayUsers.listUsers.forEach((user) => {
-            console.log(user.cpf);
-        });
-    }
-
-    function clearInputs() {
-        document.getElementById("name").value == ""
-        document.getElementById("birthdate").value == ""
-        document.getElementById("address").value == ""
-        document.getElementById("phone") == ""
-        document.getElementById("email").value == ""
-        document.getElementById("cpf").value == "";
-    }
-
-    function showUsers() {
-        document.getElementById("sub-div").classList.remove("hidden");
-        document.getElementById("main-div").classList.add("hidden");
-    }
-
-    function showRegister() {
-        document.getElementById("sub-div").classList.add("hidden");
-        document.getElementById("main-div").classList.remove("hidden");
-    }
-
-
-    function possibleClient(age) {
-        if (age == 18 && age <= 31) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    function countUsers() {
-        document.getElementById("contador").innerHTML = `Contador: ${arrayUsers.listUsers.length}`
-    }
-
-    function showList() {
-        arrayUsers.listUsers.forEach(user => {
-        document.getElementById("sub-div").innerHTML = `
+function showList() {
+    document.getElementById("user-list").classList.add("list-eachUser")
+    arrayUsers.listUsers.forEach(user => {
+        document.getElementById("user-list").innerHTML = `
             Nome: ${user.name}
             Idade: ${user.age}
             Signo: ${user.sign}
@@ -213,28 +217,28 @@ function createUser() {
             Celular: ${formatedCellphone(user.cell)}
             CPF: ${formatedCPF(user.cpf)}
         `
-        });
-    }
-    
+    });
+}
 
-    function formatedCPF(cpf) {
-        console.log("Passou pela funcao formatedCPF()");
-    
-        let cpfArray = cpf.split("");
-        let cpfFormated = cpfArray[0] + cpfArray[1] + cpfArray[2]
-            + "." + cpfArray[3] + cpfArray[4] + cpfArray[5] + "."
-            + cpfArray[6] + cpfArray[7] + cpfArray[8] + "-" + cpfArray[9] + cpfArray[10];
-        return cpfFormated;
-    }
 
-    function formatedCellphone(cellphone) {
-        console.log("Passou pela funcao formatedCellphone()");
-    
-        let cellphoneArray = cellphone.split("");
-        let cellphoneFormated = "(" + cellphoneArray[0] + cellphoneArray[1] + ")"
-            + " " + cellphoneArray[2] + cellphoneArray[3] + cellphoneArray[4]
-            + cellphoneArray[5] + cellphoneArray[6] + "-"
-            + cellphoneArray[7] + cellphoneArray[8]
-            + cellphoneArray[9] + cellphoneArray[10];
-        return cellphoneFormated;
-    }
+function formatedCPF(cpf) {
+    console.log("Passou pela funcao formatedCPF()");
+
+    let cpfArray = cpf.split("");
+    let cpfFormated = cpfArray[0] + cpfArray[1] + cpfArray[2]
+        + "." + cpfArray[3] + cpfArray[4] + cpfArray[5] + "."
+        + cpfArray[6] + cpfArray[7] + cpfArray[8] + "-" + cpfArray[9] + cpfArray[10];
+    return cpfFormated;
+}
+
+function formatedCellphone(cellphone) {
+    console.log("Passou pela funcao formatedCellphone()");
+
+    let cellphoneArray = cellphone.split("");
+    let cellphoneFormated = "(" + cellphoneArray[0] + cellphoneArray[1] + ")"
+        + " " + cellphoneArray[2] + cellphoneArray[3] + cellphoneArray[4]
+        + cellphoneArray[5] + cellphoneArray[6] + "-"
+        + cellphoneArray[7] + cellphoneArray[8]
+        + cellphoneArray[9] + cellphoneArray[10];
+    return cellphoneFormated;
+}
